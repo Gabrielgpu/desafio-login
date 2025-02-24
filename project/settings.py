d9f028f93a99acd6e12e54fbe02affcfeb398179
gabriel <gabriel_gpu@outlook.com>
2025-02-24 15:45:10 -0300

    Alterações na configuração de e-mail e criação do arquivo requirements.txt

--- project/settings.py
+++ project/settings.py
@@ -114,8 +114,19 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+if DEBUG:
+  EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+else:
+  EMAIL_BACKEND= "django.core.mail.backends.smtp.EmailBackend"
+
 
 MESSAGE_TAGS={
     messages.ERROR:'danger',
 }
+
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "SeuEmail@gmail.com"
+EMAIL_HOST_PASSWORD="Sua Senha"
