fdcc5385bc01eb02f33221cd1e94a0c311b1dba6
gabriel <gabriel_gpu@outlook.com>
2025-02-24 12:31:52 -0300

    Configuração de permissão para envio de e-mails via terminal e ajuste nos acessos ao /admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
