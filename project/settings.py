62895b6a3c63660b96a8e733bf872ec04dd1182c
gabriel <gabriel_gpu@outlook.com>
2025-02-24 14:44:21 -0300

    Alteração das mensagens de erro para tornar os alerts dinâmicos

--- project/settings.py
+++ project/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -114,3 +115,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+MESSAGE_TAGS={
+    messages.ERROR:'danger',
+}
